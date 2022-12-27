11125de67c7f6f77dc5b6f47287872d1336f16c6
ramoncordeiro <ramonestii@gmail.com>
2022-12-27 16:52:41 -0300

    uploading files

---
+++ constants/index.js
@@ -0,0 +1,9 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x9fb899a0d348fbeb760219d3045c4ff34517d04a";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
+
+
+
+
+
